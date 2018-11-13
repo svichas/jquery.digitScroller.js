68dd6b1fd91f6851714123e9488d5dd9298c405d
Stefanos Vichas <stefanosvchs@gmail.com>
2018-11-13 23:44:59 +0200

    First release

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 
 const gulp = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sass = require("gulp-sass");
 
@@ -22,29 +21,27 @@ const scssfiles = [
 gulp.task('js-concat', function() {
 
   gulp.src(jsfiles)
-  .pipe(concat('main.js'))
   .pipe(rename('jquery.digitScroller.js'))
   .pipe(gulp.dest('dist/'));
 
-  gulp.src(jsfiles)
-  .pipe(concat('main.js'))
-  .pipe(uglify())
-  .pipe(rename('jquery.digitScroller.min.js'))
-  .pipe(gulp.dest('dist/'));
+  // gulp.src(jsfiles)
+  // .pipe(uglify())
+  // .pipe(rename('jquery.digitScroller.min.js'))
+  // .pipe(gulp.dest('dist/'));
 
 });
 
 gulp.task('scss-compile', function() {
 
-  gulp.src(files)
-  .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
-  .pipe(rename('jquery.digitScroller.min.css'))
-  .pipe(gulp.dest('dist/'));
-
-  gulp.src(files)
+  gulp.src(scssfiles)
   .pipe(sass({outputStyle: "expanted"}).on("error", sass.logError))
   .pipe(rename('jquery.digitScroller.css'))
   .pipe(gulp.dest('dist/'));
+  //
+  // gulp.src(scssfiles)
+  // .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+  // .pipe(rename('jquery.digitScroller.min.css'))
+  // .pipe(gulp.dest('dist/'));
 
 });
 
