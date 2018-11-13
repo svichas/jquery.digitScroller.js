e892325483cec32ad9442a1d1aba5087d948b426
Stefanos Vichas <stefanosvchs@gmail.com>
2018-11-14 00:05:58 +0200

    Minor updates + added comments

--- dist/jquery.digitScroller.js
+++ dist/jquery.digitScroller.js
@@ -6,34 +6,42 @@ $.fn.digitScroll = function(options) {
   options.scrollTo = typeof options.scrollTo === "undefined" ? 0 : options.scrollTo;
   options.changeDur = typeof options.changeDur === "undefined" ? 0 : options.changeDur;
 
-  // get current value
+  // inits variables
   var $this = $(this);
   var tempString = $this.html();
   var tempElements = [];
   var digitScrollerLength = tempString.length;
 
-  $this.addClass("__digit_scroller_wrap");
-  $this.html("");
 
-  for (var i = 0; i < tempString.length; i++) {
-    var tempElement = $("<span/>", {
-      class: "__digit_scroller_digit"
-    });
+  if (!$this.hasClass('__digit_scroller_wrap')) {
 
-    $("<span/>", {
-      class: "__digit_scroller_current_digit",
-      html: tempString.charAt(i)
-    }).appendTo(tempElement);
+    // format element stracture
+    $this.addClass("__digit_scroller_wrap");
+    $this.html("");
 
-    $("<span/>", {
-      class: "__digit_scroller_next_digit",
-      html: tempString.charAt(i)
-    }).appendTo(tempElement);
+    for (var i = 0; i < tempString.length; i++) {
+      var tempElement = $("<span/>", {
+        class: "__digit_scroller_digit"
+      });
 
-    tempElement.appendTo($this);
+      $("<span/>", {
+        class: "__digit_scroller_current_digit",
+        html: tempString.charAt(i)
+      }).appendTo(tempElement);
+
+      $("<span/>", {
+        class: "__digit_scroller_next_digit",
+        html: tempString.charAt(i)
+      }).appendTo(tempElement);
+
+      tempElement.appendTo($this);
+    }
+
   }
 
-
+  /**
+  * Core method that controls when to update digits
+  */
   function digitScrollEvent() {
     var currentValue = getCurrentValue();
 
@@ -45,12 +53,18 @@ $.fn.digitScroll = function(options) {
     updateNextValue(currentValue);
   }
 
+  /**
+  * Methos to go to top with animation
+  */
   function goUp(digitPos) {
     var _digit = $this.find(".__digit_scroller_digit").eq(digitPos);
     // _digit.css("transition", "transform "+changeDur+"ms ease");
     _digit.css("transition", "transform " + options.changeDur + "ms ease 0s").addClass("_digit_up");
   }
 
+  /**
+  * Methos to get current digit value
+  */
   function getCurrentValue() {
     var currentValueString = "";
     $this.find('.__digit_scroller_digit').each(function() {
@@ -59,6 +73,10 @@ $.fn.digitScroll = function(options) {
     return parseInt(currentValueString);
   }
 
+
+  /**
+  * Methos to update next values
+  */
   function updateNextValue(newValue) {
     newValue = bulkDigits(newValue);
     var count = 0;
@@ -76,16 +94,24 @@ $.fn.digitScroll = function(options) {
     setTimeout(setNextToCurrent,  options.changeDur - 20);
   }
 
+  /**
+  * Methos to set next values to current digits
+  */
   function setNextToCurrent() {
 
+    // setting next to current
     $this.find('.__digit_scroller_digit').each(function() {
       $(this).find(".__digit_scroller_current_digit").html($(this).find(".__digit_scroller_next_digit").html());
     });
 
+    // move current to show it and next to bottom without animating it
     $this.find(".__digit_scroller_digit").css("transition", "transform 0ms ease 0s").removeClass('_digit_up');
 
   }
 
+  /**
+  * Method to fill gaps in a number by adding zeros
+  */
   function bulkDigits(val) {
     val = String(val);
     if (val.length >= digitScrollerLength) return val;
