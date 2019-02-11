651bbdf321b157e5018bb8e0bae1afa70a0f5df9
Stefanos Vichas <stefanos@vichas.org>
2019-02-11 20:37:59 +0200

    Changes #2 updates

--- dist/jquery.digitScroller.js
+++ dist/jquery.digitScroller.js
@@ -19,17 +19,82 @@
 
           this.init = function() {
                this.initDom();
-
-
                return this;
           }
 
 
-          this.addZeros = function(number, length) {
-               
+          /**
+          * Core method that controls when to update digits
+          */
+          this.coreScroll = function() {
+               var currentValue = getCurrentValue();
+
+               if (currentValue == opts.scrollTo) {
+                    scrolling = false;
+                    clearInterval(digitScrollEventTimer) // clear interval
+                    return;
+               }
+
+               if (opts.scrollTo > currentValue) {
+                    currentValue++;
+               } else {
+                    currentValue--;
+               }
+
+               updateNextValue(currentValue);
           }
 
 
+          this.getCurrentValue = function() {
+               var currentValueString = 0;
+               $this.find('.__digit_scroller_digit').each(function() {
+                    currentValueString += $(this).find(".__digit_scroller_current_digit").html();
+               });
+               return parseInt(removeFrontZeros(currentValueString));
+          }
+
+          /**
+          * Method to go to top with animation
+          */
+          this.transitionToUp = function(digitPos) {
+               $this.find(".__digit_scroller_digit").eq(digitPos)
+               .css("transition", "transform " + opts.scrollDuration + "ms ease")
+               .addClass("_digit_up");
+          }
+
+          /**
+          * Method to set next values to current values
+          */
+          this.setNextNumberToCurrent = function() {
+
+               // setting next to current
+               $this.find('.__digit_scroller_digit').each(function() {
+                    $(this).find(".__digit_scroller_current_digit").html($(this).find(".__digit_scroller_next_digit").html());
+               });
+
+               // move current to show it and next to bottom without animating it
+               $this.find(".__digit_scroller_digit")
+               .css("transition", "transform 0ms ease 0s")
+               .removeClass('_digit_up');
+
+               return true;
+          }
+
+
+          this.addZeros = function(number, length) {
+               number = String(number);
+               if (number.length >= length) return number;
+               for (var i=number.length;i<length;i++) {
+                    number = "0" + number;
+               }
+               return number;
+          }
+
+          this.removeFrontZeros = function(currentValueString) {
+               if (!currentValueString.includes("-")) return currentValueString;
+               return currentValueString.replace(new RegExp("^[0]*"), '');
+          }
+          
           /**
           * Method to format digitScroller
           */
@@ -62,7 +127,6 @@
                          "html": num
                     }));
           }
-
 
 
           // public methods
